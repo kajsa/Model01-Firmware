5ee79d884bc719d1ac6ee48b0d81b0125caade4a
Kajsa Anderson <kajsa.anderson@thomsonreuters.com>
2017-10-30 17:31:24 -0500

    Kajsa's customizations to modifiers & symbols

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -12,14 +12,13 @@
 #include "Kaleidoscope-MouseKeys.h"
 #include "Kaleidoscope-Macros.h"
 #include "Kaleidoscope-LEDControl.h"
-#include "Kaleidoscope-Numlock.h"
+//#include "Kaleidoscope-Numlock.h"
 #include "Kaleidoscope.h"
 
 #include "LED-Off.h"
 #include "Kaleidoscope-LEDEffect-BootGreeting.h"
 #include "Kaleidoscope-LEDEffect-SolidColor.h"
 #include "Kaleidoscope-LEDEffect-Breathe.h"
-#include "Kaleidoscope-LEDEffect-Chase.h"
 #include "Kaleidoscope-LEDEffect-Rainbow.h"
 #include "Kaleidoscope-LED-Stalker.h"
 #include "Kaleidoscope-LED-AlphaSquare.h"
@@ -32,21 +31,20 @@
 #define NUMPAD_KEYMAP 2
 
 #define GENERIC_FN2  KEYMAP_STACKED ( \
-___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           XXX,         \
-Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE, \
-Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,                   \
-Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE, \
-                                            Key_LeftControl, Key_Delete, Key_LeftGui, Key_LeftShift,  \
-                                                                ___,   \
+___,     Key_F1,          Key_F2,        Key_F3,        Key_F4,        Key_F5,           Key_LEDEffectNext, \
+Key_Tab, Key_PrintScreen, Key_Insert,    Key_mouseUp,   ___,           Key_mouseWarpEnd, Key_mouseWarpNE,   \
+Key_PageUp, Key_ScrollLock, Key_mouseL,  Key_mouseDn,   Key_mouseR,    Key_mouseWarpNW,                     \
+Key_PageDown, Key_Pause,  Key_mouseBtnL, Key_mouseBtnM, Key_mouseBtnR, Key_mouseWarpSW,  Key_mouseWarpSE,   \
+Key_Delete, ___, ___, ___,  \
+            ___,   \
 \
 Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                  Key_F9,          Key_F10,          Key_F11, \
 Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,   Key_LeftBracket, Key_RightBracket, Key_F12, \
                             Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,             Key_RightArrow,  ___,              ___, \
 Key_PcApplication,          Key_Mute,               Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,            Key_Backslash,    Key_Pipe, \
-Key_RightShift, Key_RightAlt, Key_Enter, Key_RightControl, \
+___, ___, ___, Key_Enter, \
 ___ \
 )
-
 
 
 #define NUMPAD KEYMAP_STACKED  (\
@@ -67,18 +65,18 @@ ___ \
 )
 
 #define QWERTY KEYMAP_STACKED ( \
-    ___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext, \
+    ___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_6, \
     Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,           \
-    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,                    \
-    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,        \
-    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,         \
+    Key_Home,   Key_A, Key_S, Key_D, Key_F, Key_G,                    \
+    Key_End, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,        \
+    Key_Backspace, Key_LeftShift, Key_LeftGui, Key_LeftControl,         \
                           Key_Keymap1_Momentary,     \
 \
-    Macro_Any,       Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_ToggleNumlock, \
-    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,       \
-                   Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,       \
-    Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,       \
-    Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,                   \
+    Macro_Any,     Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_CapsLock, \
+    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_LeftBracket,    \
+                   Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,     \
+    Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_RightBracket,  \
+    Key_RightControl, Key_LeftAlt, Key_RightShift, Key_Spacebar,                      \
     Key_Keymap1_Momentary \
 )
 
@@ -97,9 +95,7 @@ static kaleidoscope::LEDSolidColor solidIndigo(0, 0, 170);
 static kaleidoscope::LEDSolidColor solidViolet(130, 0, 120);
 
 const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
-  if (macroIndex == TOGGLENUMLOCK && keyToggledOn(keyState)) {
-    return NumLock.toggle();
-  } else if (macroIndex == 1 && keyToggledOn(keyState)) {
+  if (macroIndex == 1 && keyToggledOn(keyState)) {
     Macros.type(PSTR("Keyboardio Model 01 - Kaleidoscope "));
     Macros.type(PSTR(BUILD_INFORMATION));
   } else if (macroIndex == MACRO_ANY) {
@@ -120,17 +116,17 @@ void setup() {
     &BootGreetingEffect,
     &TestMode,
     &LEDControl, &LEDOff,
-    &LEDRainbowEffect, &LEDRainbowWaveEffect, &LEDChaseEffect,
+    &LEDRainbowEffect, &LEDRainbowWaveEffect, 
     &solidRed, &solidOrange, &solidYellow, &solidGreen, &solidBlue, &solidIndigo, &solidViolet,
     &LEDBreatheEffect,
     &AlphaSquareEffect,
     &StalkerEffect,
-    &NumLock,
+//    &NumLock,
     &Macros,
     &MouseKeys,
     NULL);
 
-  NumLock.numPadLayer = NUMPAD_KEYMAP;
+//  NumLock.numPadLayer = NUMPAD_KEYMAP;
   AlphaSquare.color = { 255, 0, 0 };
   LEDRainbowEffect.brightness(150);
   LEDRainbowWaveEffect.brightness(150);
